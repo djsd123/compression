18f005ab8092618e50cee091fa8e60c37accffa2
Mikael <mikellallison@gmail.com>
2017-01-09 09:59:56 +0000

    Initial commit. Simple tool for compressing files on the command line

---
+++ simple_gzip_parallel_compression.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup //Wait groups don't need to be initialised
+	var i int = -1        //Because we want to reference i outside the for loop, we declare it here
+	var file string
+	for i, file = range os.Args[1:] {
+		wg.Add(1)
+		go func(filename string) {
+			compress(filename)
+			wg.Done()
+		}(file)
+	}
+	wg.Wait()
+	fmt.Printf("Compressed %d files\n", i+1)
+}
+
+func compress (filename string) error {
+	in, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(filename + ".gz")
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	gzout := gzip.NewWriter(out)
+	_, err = io.Copy(gzout, in)
+	gzout.Close()
+
+	return err
+}
